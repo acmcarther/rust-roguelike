26fa37701951764064f7544748feb9bcedc2094f
Alex McArther <acmcarther@gmail.com>
2015-03-11 19:39:06 -0700

    Cleaned up game logic and wait loop

--- game_logic/src/main_logic/mod.rs
+++ game_logic/src/main_logic/mod.rs
@@ -1,4 +1,4 @@
-use std::old_io::stdin;
+use std::old_io::{stdin, IoResult};
 use std::sync::mpsc::channel;
 use std::thread;
 use std::num::ToPrimitive;
@@ -7,43 +7,50 @@ use time;
 use menus::pause_screen;
 use shared_types::MenuResult;
 
+#[derive(PartialEq, Eq)]
+enum GameState {
+  RecvInput,
+  Waiting,
+  Finished
+}
+
 pub fn start_game() {
-  let mut continuing = true;
-  while continuing {
+  let mut state = GameState::RecvInput;
+  while state == GameState::RecvInput {
     let (tx, rx) = channel();
     thread::spawn(move|| {
       tx.send(stdin().read_char()).unwrap();
     });
 
-    let mut waiting = true;
     let mut last_len = 0usize;
     let mut last_time = time::now();
+    state = GameState::Waiting;
 
-    while waiting {
+    while state == GameState::Waiting {
       let this_time = time::now();
       last_len = print_stuff(last_len, last_time, this_time);
       last_time = this_time;
-      match rx.try_recv() {
-        Ok(user_input) =>  {
-          print!("\n");
-          waiting = false;
-          match user_input {
-            Ok(input) => {
-              if input == 'q' {
-                continuing = pausing()
-              }
-            },
-            _ => ()
-          }
-
-        },
-        _ => ()
-      }
+      state = rx.try_recv().map(check_pausing).unwrap_or_else(|_| GameState::Waiting);
     }
+  }
+
+}
+
+fn check_pausing(user_input: IoResult<char>) -> GameState {
+  user_input.map(check_pause_game_state)
+            .unwrap_or_else(|_| GameState::RecvInput)
+}
+
+fn check_pause_game_state(input: char) -> GameState {
+  if input == 'q' {
+    pausing()
+  } else {
+    GameState::RecvInput
   }
 }
 
-pub fn print_stuff(last_len: usize, last_time: time::Tm, this_time: time::Tm) -> usize {
+
+fn print_stuff(last_len: usize, last_time: time::Tm, this_time: time::Tm) -> usize {
   let dt = this_time.tm_nsec - last_time.tm_nsec;
   let freq = 1.0 / dt.to_f32().unwrap();
   for _ in 0..last_len {
@@ -54,13 +61,13 @@ pub fn print_stuff(last_len: usize, last_time: time::Tm, this_time: time::Tm) ->
   res_str.len()
 }
 
-pub fn pausing() -> bool {
+fn pausing() -> GameState {
   match pause_screen::launch_screen() {
     MenuResult::Options => {
       println!("No options");
       pausing()
     },
-    MenuResult::Play => true,
-    MenuResult::Exit => false
+    MenuResult::Play => GameState::RecvInput,
+    MenuResult::Exit => GameState::Finished
   }
 }
